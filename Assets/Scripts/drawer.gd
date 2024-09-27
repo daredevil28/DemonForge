69561d5474adad5df2b9917d72ab13214ce87679
daredevil28 <8192966+daredevil28@users.noreply.github.com>
2024-09-27 04:02:35 +0200

    Put persistent lines in it's own script. Fix note settings not getting translated

--- Assets/Scripts/drawer.gd
+++ Assets/Scripts/drawer.gd
@@ -7,14 +7,8 @@ func _init() -> void:
 
 
 func _draw() -> void:
-	# The judgement line
 	var line_color : Color = Color.WHITE
-	draw_line(Vector2(NoteManager.offset, 0), Vector2(NoteManager.offset,DisplayServer.window_get_size().y), line_color, 4)
-	
-	# The note lines
-	for i : int in range(1,7):
-		draw_line(Vector2(0,NoteManager.get_note_lane_y(i)),Vector2(DisplayServer.window_get_size().x,NoteManager.get_note_lane_y(i)),line_color,1)
-	
+
 	# Beat markers
 	# Loop for each marker
 	for x : int in range(0,NoteManager.marker_nodes.size()):
---
+++ Assets/Scripts/persistent_lines.gd
@@ -0,0 +1,14 @@
+extends Node2D
+
+func _ready() -> void:
+	queue_redraw()
+	get_tree().get_root().size_changed.connect(queue_redraw)
+	
+func _draw() -> void:
+	# The judgement line
+	var line_color : Color = Color.WHITE
+	draw_line(Vector2(NoteManager.offset, 0), Vector2(NoteManager.offset,DisplayServer.window_get_size().y), line_color, 4)
+	
+	# The note lines
+	for i : int in range(1,7):
+		draw_line(Vector2(0,NoteManager.get_note_lane_y(i)),Vector2(DisplayServer.window_get_size().x,NoteManager.get_note_lane_y(i)),line_color,1)
